ccb61b75290f989315bef03766aa36629222111c
Pee <peeratchta@gmail.com>
2016-01-04 15:16:58 +0700

    change out to in

--- Backend/header.php
+++ Backend/header.php
@@ -29,7 +29,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="../disconect.php">Log out</a></li>
+                    <li><a href="../disconect.php">Log in</a></li>
 
                 </ul>
             </div><!-- /.navbar-collapse -->
