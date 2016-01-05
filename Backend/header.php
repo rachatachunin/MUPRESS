7ce5e43d0ca7903ac3465b1845b38abe11046525
Pee <peeratchta@gmail.com>
2016-01-05 15:37:55 +0700

    stock and header

--- Backend/header.php
+++ Backend/header.php
@@ -8,8 +8,18 @@
 
  echo '
 <header>
+  <style>
+        .navbar .navbar-nav {
+        display: inline-block;
+        float: none;
+        }
+
+        .navbar .navbar-collapse {
+        text-align: center;
+        }
+  </style>
     <nav id="navbarC" class="navbar navbar-default" role = "navigation">
-        <div class="container-fluid">
+        <div class="container-fluid text-center">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -22,19 +32,22 @@
                     <img class="img-responsive" alt="Brand" width="130px"  src="/MUPRESS_GIT/image/logo_mupress.jpg">
                 </a>
             </div>
-            <div class="text-center" style="margin-left:420px;" >
+
+            <!-- <div class="nav navbar-nav" >
                     <a href="/MUPRESS_GIT/Backend/admin.php" class="navbar-text"> <h4>MUPMIS</h4><b>(Mahidol University Press Management Information System)</b></a></li>
-            </div>
+            </div> -->
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-
+                <div class="nav navbar-nav" >
+                        <a href="/MUPRESS_GIT/Backend/admin.php" class="navbar-text"> <h4>MUPMIS</h4><b>(Mahidol University Press Management Information System)</b></a></li>
+                </div>
+                <!-- <ul class="nav navbar-nav navbar-right">
                     <li><a href="/MUPRESS_GIT/disconnect.php">Log out</a></li>
-
-
-
-                </ul>
+                </ul> -->
+                <div class="nav navbar-nav navbar-right" style="margin-top:30px;">
+                    <a href="/MUPRESS_GIT/disconnect.php" role="button" class="btn btn-danger">Log out</a>
+                </div>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
--- head.php
+++ head.php
@@ -1,4 +1,8 @@
 <?php echo '<link href="/MUPRESS_GIT/css/bootstrap.min.css" rel="stylesheet">
 <link href="/MUPRESS_GIT/css/index.css" rel="stylesheet">
+<link href="/MUPRESS_GIT/bower-chosen-master/chosen.css" rel="stylesheet">
+<link href="/MUPRESS_GIT/css/bootstap-chosen.css" rel="stylesheet">
 <script src="/MUPRESS_GIT/js/jq.js"></script>
-<script src="/MUPRESS_GIT/js/bootstrap.min.js"></script>';
+<script src="/MUPRESS_GIT/js/bootstrap.min.js"></script>
+<script src="/MUPRESS_GIT/bower-chosen-master/chosen.jquery.js"></script>'
+;
