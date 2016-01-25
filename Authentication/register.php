fb71fb13f077d5c17103e021f02074b28dd088f3
Natural <supanat.nature@gmail.com>
2016-01-25 15:36:32 +0700

    authentication

--- Authentication/register.php
+++ Authentication/register.php
@@ -1,17 +1,20 @@
 <?php
 
-
 include "../dbconnection.php";
-
+ // var_dump($_POST['fname']);
 $fn = $_POST['fname'];
 $ln = $_POST['lname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $tel = $_POST['tel'];
+$address = $_POST['address'];
+$gender = $_POST['gender'] ;
 
-$sql="INSERT into user(email,password,user_fn,user_ln,tel,user_type) values ('".$email."','".$password."','".$fn."','".$ln."','".$tel."',2)";
+
+$sql="INSERT into user(email,password,user_fn,user_ln,gender,tel,address,user_type) values ('".$email."','".$password."','".$fn."','".$ln."','".$gender."','".$address."','".$tel."',2)";
+// var_dump($sql);
 mysqli_query($con,$sql);
 
-mysql_close();
+// mysql_close();
 
 header("Location: ../index.php");
