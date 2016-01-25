ffd283701e11226d67be08ad701274b345ad8e21
Natural <supanat.nature@gmail.com>
2016-01-25 20:42:11 +0700

    change

--- head.php
+++ head.php
@@ -2,6 +2,10 @@
 <link href="/MUPRESS_GIT/css/index.css" rel="stylesheet">
 <link href="/MUPRESS_GIT/bower-chosen-master/chosen.css" rel="stylesheet">
 <link href="/MUPRESS_GIT/css/bootstap-chosen.css" rel="stylesheet">
+<link href="/MUPRESS_GIT/css/clean-blog.min.css" rel="stylesheet">
+<link href="/MUPRESS_GIT/fonts/font1.css" rel="stylesheet">
+<link href="/MUPRESS_GIT/fonts/font2.css" rel="stylesheet">
+<link href="/MUPRESS_GIT/fonts/font_awesome.css" rel="stylesheet">
 <script src="/MUPRESS_GIT/js/jq.js"></script>
 <script src="/MUPRESS_GIT/js/list.js"></script>
 <script src="/MUPRESS_GIT/js/bootstrap.min.js"></script>
---
+++ listAllBooks.php
@@ -0,0 +1,87 @@
+<?
+ob_start();
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8">
+    <?php
+     include "head.php"
+     ?>
+
+    <!--<link href="css/bootstrap-theme.min.css" rel="stylesheet">-->
+    <title>MU PRESS</title>
+</head>
+
+<body>
+  <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#" data-toggle="modal" data-target="#L_R"><h4>สมัครสมาชิก หรือ ลงชื่อเข้าใช้งาน</h4></a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('image/home-bg.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>สำนักพิมพ์ มหาวิทยาลัย มหิดล</h1>
+                        <hr class="small">
+                        <span class="subheading">Mahidol University Press</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 ">
+                <div class="">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav" style="text-color:white">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="post.html">Sample Post</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+              </div>
+            </div>
+              </div>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
