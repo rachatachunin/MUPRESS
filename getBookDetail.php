20c41ec34bd0a48b04adf14e8d6de9d3b2dcf764
Natural <supanat.nature@gmail.com>
2016-01-26 20:24:55 +0700

    listallbook

---
+++ getBookDetail.php
@@ -0,0 +1,29 @@
+<?php
+
+$db_cont = "dbconnection.php";
+
+if(!@file_exists("./".$db_cont) ) {
+    echo 'can not include db_cont.php';
+} else {
+   require("./".$db_cont);
+}
+
+$pageNum 	= $_GET['pageNumber'];
+unset($_GET['pageNumber']);
+
+if($pageNum == 1){
+$sql = "SELECT * FROM book ORDER BY book_id ASC LIMIT 0,21";
+}else if($pageNum == 2){
+$sql = "SELECT * FROM book ORDER BY book_id ASC LIMIT 21,41";
+}
+
+$result = mysqli_query($con,$sql);
+$data = array();
+while($r = mysqli_fetch_assoc($result)) {
+    $data[] = $r;
+}
+mysqli_close($con);
+//print_r($data);
+
+echo json_encode($data);
+ ?>
--- listAllBooks.php
+++ listAllBooks.php
@@ -14,9 +14,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         include "headFrontEnd.php";
     ?>
     <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-    <script src="/MUPRESSjs/simpleCart.min.js"> </script>
-    <link href="/MUPRESS/css/memenu.css" rel="stylesheet" type="text/css" media="all" />
-    <script type="text/javascript" src="/MUPRESSjs/memenu.js"></script>
+    <!-- <link href="/MUPRESS/css/simplePagination.css" rel="stylesheet" type="text/css" media="all" /> -->
+    <script src="/MUPRESS_GIT/js/simpleCart.min.js"> </script>
+    <link href="/MUPRESS_GIT/css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+    <script type="text/javascript" src="/MUPRESS_GIT/js/memenu.js"></script>
+      <script type="text/javascript" src="/MUPRESS_GIT/js/jquery.bootpag.min.js"></script>
+    <!-- <script src="//raw.github.com/botmonster/jquery-bootpag/master/lib/jquery.bootpag.min.js"></script> -->
     <script>$(document).ready(function(){$(".memenu").memenu();});</script>
 
     <title>MU PRESS</title>
@@ -25,11 +28,54 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
   <?php
       include "header.php";
   ?>
+<div class="container-fluid">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+          <div class="item active text-center">
+              <!--<img src="image/behavioralgame.jpg" alt="Chania" >-->
+              <h3 style="width: 200px;height: 200px;">Promotion</h3>
+          </div>
+
+          <div class="item">
+              <img src="image/health.jpg" alt="Chania" >
+          </div>
+
+          <div class="item">
+              <img src="image/law.jpg" alt="Flower" >
+          </div>
+
+          <div class="item">
+              <img src="image/rome.jpg" alt="Flower" >
+          </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+      </a>
+  </div>
+</div>
+
   <!--product-starts-->
-	<div class="product">
+	<div class="product" >
 		<div class="container">
-			<div class="product-top">
-				<div class="product-one">
+
+			<div class="product-top" id="showProducts">
+				<!-- <div class="product-one">
 					<div class="col-md-3 product-left">
 						<div class="product-main simpleCart_shelfItem">
 							<a href="single.html" class="mask"><img class="img-responsive zoom-img" src="image/p-1.png" alt="" /></a>
@@ -83,12 +129,120 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						</div>
 					</div>
 					<div class="clearfix"></div>
-				</div>
+				</div> -->
+
+
+
 				</div>
 			</div>
-		</div>
 	</div>
 	<!--product-end-->
+
+<!-- pagina-start  -->
+
+                <div class="col-xs-12">
+                  <div id="content"></div>
+                  <div id="page-selection" class="text-center"></div>
+                      <script>
+                        window.onload = function(){
+                          getAjax(1);
+                        };
+                          // init bootpag
+                          $('#page-selection').bootpag({
+                            total: 2,
+                            page: 1,
+                            maxVisible: 5,
+                            leaps: true,
+                            firstLastUse: true,
+                            first: '←',
+                            last: '→',
+                            wrapClass: 'pagination',
+                            activeClass: 'active',
+                            disabledClass: 'disabled',
+                            nextClass: 'next',
+                            prevClass: 'prev',
+                            lastClass: 'last',
+                            firstClass: 'first'
+                          }).on("page", function(event, num){
+                            getAjax(num);
+                       });
+
+                          function getAjax(num){
+
+                            $.ajax({
+                           url: "getBookDetail.php",
+                           data: {
+                               pageNumber: num ,
+                           },
+                           type: "GET",
+                           dataType : "JSON",
+                           success: function( json ) {
+                             var bookArray = json ;
+                             showAllbook(bookArray);
+                           },
+                           error: function( xhr, status, errorThrown ) {
+                               alert( "Sorry, there was a problem in user profile request!" );
+                               console.log( "Error: " + errorThrown );
+                               console.log( "Status: " + status );
+                               console.dir( xhr );
+                           },
+                         });
+                          }
+                          function showAllbook(data){
+                            // first row
+                             var products = "";
+                             var counter = 0 ;
+                          for(var j = 0 ; j < 5  ; j++){
+                            if(counter >= data.length) {break;}
+                            products += "<div class='product-top'>";
+                            for(var i = 0 ; i < 4 ;i++) {
+                              // products += data[i]['title'] ;
+                               products += "<div class='col-md-3 product-left'>" ;
+                               products += "<div class='product-main simpleCart_shelfItem' style='padding: 8px;'>";
+
+                               products += "<a href='#' class='mask'><img class='img-responsive zoom-img' src='image/p-3.png' alt='' /></a> ";
+                               products += "<div class='product-bottom'>";
+                               products += "<h3>" + data[counter]['title'] + "</h3>" ;
+               							   products +=	"<p>" + "ผู้เขียน " + data[counter]['author'] + "</p>" ;
+                               products +=	"<p>" + "serial no. " + data[counter]['serial_no'] + "</p>" ;
+                               products +=	"<p>" + "Edition  " + data[counter]['edition'] + "</p>" ;
+                               products += "<h4><a class='item_add' href='#'><i></i></a> <span class='item_price'>" + data[counter]['price'] +" บาท"+"</span></h4>"  ;
+                               products += "</div>";
+
+                               products += "<div class='srch'>";
+                               products += "<span>" + "-50" + "</span>" ;
+                               products += "</div>";
+
+                               products += "</div>";
+                               products += "</div>";
+                               counter ++ ;
+                               if(counter >= data.length) {break;}
+                             }
+                              products += "<div class='clearfix'></div> ";
+                              products +="</div><br>" ;
+
+                          }
+
+                            $("#showProducts").html(products);
+                          }
+                      </script>
+                </div>
+
+  <!-- pagina-end -->
+
+  <!--footer-starts-->
+  	<div class="footer">
+  		<div class="container">
+  			<div class="footer-top">
+
+  				<div class="col-md-12 footer-right">
+  					<p>© 2015 Mahidol University Press. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">Mahidol Students</a> </p>
+  				</div>
+  				<div class="clearfix"></div>
+  			</div>
+  		</div>
+  	</div>
+  	<!--footer-end-->
 
 </body>
 
