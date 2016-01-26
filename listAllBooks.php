5e4c5573ededf4762cf748ea3efd43714c7987ec
Natural <supanat.nature@gmail.com>
2016-01-26 14:54:24 +0700

    big change

--- listAllBooks.php
+++ listAllBooks.php
@@ -6,19 +6,89 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-Type" content="text/html; charset=utf-8">
-
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+ <meta name="keywords" content="Luxury Watches Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
     <?php
         include "headFrontEnd.php";
     ?>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <script src="/MUPRESSjs/simpleCart.min.js"> </script>
+    <link href="/MUPRESS/css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+    <script type="text/javascript" src="/MUPRESSjs/memenu.js"></script>
+    <script>$(document).ready(function(){$(".memenu").memenu();});</script>
 
-    <!--<link href="css/bootstrap-theme.min.css" rel="stylesheet">-->
     <title>MU PRESS</title>
 </head>
 <body>
   <?php
       include "header.php";
   ?>
+  <!--product-starts-->
+	<div class="product">
+		<div class="container">
+			<div class="product-top">
+				<div class="product-one">
+					<div class="col-md-3 product-left">
+						<div class="product-main simpleCart_shelfItem">
+							<a href="single.html" class="mask"><img class="img-responsive zoom-img" src="image/p-1.png" alt="" /></a>
+							<div class="product-bottom">
+								<h3>Smart Watches</h3>
+								<p>Explore Now</p>
+								<h4><a class="item_add" href="#"><i></i></a> <span class=" item_price">$ 329</span></h4>
+							</div>
+							<div class="srch">
+								<span>-50%</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 product-left">
+						<div class="product-main simpleCart_shelfItem">
+							<a href="single.html" class="mask"><img class="img-responsive zoom-img" src="image/p-2.png" alt="" /></a>
+							<div class="product-bottom">
+								<h3>Smart Watches</h3>
+								<p>Explore Now</p>
+								<h4><a class="item_add" href="#"><i></i></a> <span class=" item_price">$ 329</span></h4>
+							</div>
+							<div class="srch">
+								<span>-50%</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 product-left">
+						<div class="product-main simpleCart_shelfItem">
+							<a href="single.html" class="mask"><img class="img-responsive zoom-img" src="image/p-3.png"  alt="" /></a>
+							<div class="product-bottom">
+								<h3>Smart Watches</h3>
+								<p>Explore Now</p>
+								<h4><a class="item_add" href="#"><i></i></a> <span class=" item_price">$ 329</span></h4>
+							</div>
+							<div class="srch">
+								<span>-50%</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 product-left">
+						<div class="product-main simpleCart_shelfItem">
+							<a href="single.html" class="mask"><img class="img-responsive zoom-img" src="image/p-4.png" alt="" /></a>
+							<div class="product-bottom">
+								<h3>Smart Watches</h3>
+								<p>Explore Now</p>
+								<h4><a class="item_add" href="#"><i></i></a> <span class=" item_price">$ 329</span></h4>
+							</div>
+							<div class="srch">
+								<span>-50%</span>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--product-end-->
 
 </body>
 
