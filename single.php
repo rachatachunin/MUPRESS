54008699eb1bd51bda5e271e3b334d10d0e5baeb
Natural <supanat.nature@gmail.com>
2016-01-28 08:31:35 +0700

    xxx

--- single.php
+++ single.php
@@ -13,12 +13,130 @@ $book_id = $_GET['id'];
     Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
         <?php
             include "headFrontEnd.php";
+            include "dbconnection.php";
+
+            $sqlgetBookSingle = "SELECT * FROM book WHERE book_id = '".$book_id."' ";
+
+            $result = mysqli_query($con,$sqlgetBookSingle);
+            $row = mysqli_fetch_array($result);
         ?>
     <script>$(document).ready(function(){$(".memenu").memenu();});</script>
+    <script type="text/javascript">
+    $(function() {
+
+    var menu_ul = $('.menu_drop > li > ul'),
+           menu_a  = $('.menu_drop > li > a');
+
+    menu_ul.hide();
+
+    menu_a.click(function(e) {
+        e.preventDefault();
+        if(!$(this).hasClass('active')) {
+            menu_a.removeClass('active');
+            menu_ul.filter(':visible').slideUp('normal');
+            $(this).addClass('active').next().stop(true,true).slideDown('normal');
+        } else {
+            $(this).removeClass('active');
+            $(this).next().stop(true,true).slideUp('normal');
+        }
+    });
+
+});
+    </script>
 
         <title>MU PRESS</title>
 </head>
 <body>
+  <?php include "header.php" ?>
+
+  <!--start-breadcrumbs-->
+<div class="breadcrumbs">
+  <div class="container">
+    <div class="breadcrumbs-main">
+      <ol class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li class="active">Single</li>
+      </ol>
+    </div>
+  </div>
+</div>
+<!--end-breadcrumbs-->
+
+<!--start-single-->
+	<div class="single contact">
+		<div class="container">
+			<div class="single-main">
+				<div class="col-md-12 single-main-left">
+				<div class="sngl-top">
+					<div class="col-md-5 single-top-left">
+						<div class="single-para simpleCart_shelfItem" style="height:600px;">
+									<div style="height:600px;"> <img src="image/law.jpg" style="width:370px; height:600px;" class="img-responsive" alt=""/> </div>
+						</div>
+
+					</div>
+					<div class="col-md-7 single-top-right">
+						<div class="single-para simpleCart_shelfItem">
+						<h2><?php echo $row['title']; ?></h2>
+							<div class="star-on">
+								<ul class="star-footer">
+										<li><a href="#"><i> </i></a></li>
+										<li><a href="#"><i> </i></a></li>
+										<li><a href="#"><i> </i></a></li>
+										<li><a href="#"><i> </i></a></li>
+										<li><a href="#"><i> </i></a></li>
+									</ul>
+
+							<div class="clearfix"> </div>
+							</div>
+
+							<h5 class="item_price">฿ <?php echo $row['price'] ; ?></h5>
+
+							<div class="available">
+								<ul>
+									<li>ผู้เขียน
+										<span style="margin-left:16%;"><?php echo $row['author']; ?></span>
+                </li>
+								<li>Serial no.
+                    	<span style="margin-left:16%;"><?php echo $row['serial_no']; ?></span>
+                </li>
+                <li>Edition
+                    	<span style="margin-left:130px;"><?php echo $row['edition']; ?></span>
+                </li>
+                <li>พิมพ์ครั้งที่
+                    	<span style="margin-left:102px;">1</span>
+                </li>
+								<div class="clearfix"> </div>
+							</ul>
+						</div>
+
+								<a href="#" class="add-cart item_add">ใส่ตระกร้า</a>
+
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="tabs" style="margin-top:0px;">
+					<ul class="menu_drop">
+				<li class="item1"><a href="#"><i class = "glyphicon glyphicon-book"></i>&nbsp ตัวอย่างเนื้อหา</a>
+					<ul>
+					 <li >
+					   <?php  echo $row['content_preview']; ?>
+					 </li>
+					</ul>
+				</li>
+
+	 		</ul>
+				</div>
+	</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--end-single-->
+
+  <!-- start-footer -->
+  <?php include"footer.php" ?>
+  <!-- end-footer -->
 
 </body>
 
