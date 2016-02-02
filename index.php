c2eefd66603c03389e3ee49a66c59bf47b6bb596
Pee <peeratchta@gmail.com>
2016-02-02 20:05:13 +0700

    Revert "Merge branch 'master' into me"
    This reverts commit 5766b00db3e21da251618f79c19a67d3bce583a3, reversing
    changes made to 33b7bb3c2c45248a3fc377fd98c6be87b8d9d0d3.

--- index.php
+++ index.php
@@ -171,7 +171,7 @@ $result2 = mysqli_query($con,$sql2);
             echo '<input type="text" name="name" class="item_bid" style="display: none" value="'.$row['book_id'].'">';
             echo '<input type="text" name="name" class="item_dc" style="display: none" value="18">';
             }else {
-            echo '<h4><a class="item_add" href="#" style="pointer-events:none; cursor: default;"><i></i></a> <span class="item_price">' . $row['price'] .' บาท'.'</span><span style=" margin-left:40px; color: red;"> สินค้าหมด</span></h4>'  ;
+            echo '<h4><a class="item_add" href="#"><i></i></a> <span class="item_price">' . $row['price'] .' บาท'.'</span><span style=" margin-left:40px; color: red;"> สินค้าหมด</span></h4>'  ;
             echo '<input type="text" name="name" class="item_bid" style="display: none" value="'.$row['book_id'].'">';
             echo '<input type="text" name="name" class="item_dc" style="display: none" value="18">';
             }
@@ -209,7 +209,7 @@ $result2 = mysqli_query($con,$sql2);
             echo '<input type="text" name="name" class="item_dc" style="display: none" value="18">';
             }else {
 
-            echo '<h4><a class="item_add" href="#" style="pointer-events:none; cursor: default;"><i></i></a> <span class="item_price">' . $row2['price'] .' บาท'.'</span><span style=" margin-left:40px; color: red;"> สินค้าหมด</span></h4>'  ;
+            echo '<h4><a class="item_add" href="#"><i></i></a> <span class="item_price">' . $row2['price'] .' บาท'.'</span><span style=" margin-left:40px; color: red;"> สินค้าหมด</span></h4>'  ;
             echo '<input type="text" name="name" class="item_bid" style="display: none" value="'.$row['book_id'].'">';
             echo '<input type="text" name="name" class="item_dc" style="display: none" value="18">';
             }
